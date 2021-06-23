cb0b9082b7128fd1df45cef39481877b107e5578
unknown <asad.jamil.revature.net>
2021-06-23 14:02:48 -0400

    Hello World

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld{
+    //THis is a comment
+
+    /*Multi line Comments 
+     Syntax
+    */
+    // the main metho
+    public static void main (String [] args){
+        System.out.println("Hello World");
+    }
+}
